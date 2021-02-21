40f94d047f37849b339ddd6610dbcaea82ee7406
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-02-21 10:09:29 +0100

    Add HTTP Cache ETag vs Cache Control

--- src/main/kotlin/com/nmincuzzi/ipweather/adapter/OpenWeatherMapAdapter.kt
+++ src/main/kotlin/com/nmincuzzi/ipweather/adapter/OpenWeatherMapAdapter.kt
@@ -12,7 +12,7 @@ class OpenWeatherMapAdapter {
         /*val client = OkHttpClient()
 
         val request = Request.Builder()
-                .url("https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html")
+                .url("")
                 .get()
                 .addHeader("x-rapidapi-key", "f531619183mshd2a9e647bcd2ae2p1d5b6fjsn0ec1929f8f17")
                 .addHeader("x-rapidapi-host", "community-open-weather-map.p.rapidapi.com")
