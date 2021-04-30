2db5d0a1b1bff8f83a748c118f9089cc79aad545
Nicola Mincuzzi <nicolamincuzzi88@gmail.com>
2021-04-30 23:55:11 +0200

    [chore]: Improve info about OpenAPI

--- src/main/kotlin/com/nmincuzzi/ipweather/IpWeatherApplication.kt
+++ src/main/kotlin/com/nmincuzzi/ipweather/IpWeatherApplication.kt
@@ -1,11 +1,14 @@
 package com.nmincuzzi.ipweather
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@OpenAPIDefinition(info = Info(title = "IP Weather API", version = "1.0", description = "Documentation about IP Weather endpoints."))
 class IpWeatherApplication
 
 fun main(args: Array<String>) {
-	runApplication<IpWeatherApplication>(*args)
+    runApplication<IpWeatherApplication>(*args)
 }
