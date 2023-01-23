f76689d6d9eb9eb4372bdf8f8043e79819fb6f30
Nicola Mincuzzi <48289901+NicoMincuzzi@users.noreply.github.com>
2023-01-23 19:42:56 +0100

    [chore]: Upgrade dependencies and plugins versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.spring") version "1.6.10"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.spring") version "1.7.0"
     jacoco
 }
 
@@ -18,17 +18,17 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:2.6.3")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.10")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.10")
-    implementation("org.projectlombok:lombok:1.18.16")
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
+    implementation("org.projectlombok:lombok:1.18.24")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.6.3") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
-    testImplementation("io.mockk:mockk:1.12.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.32.0")
+    testImplementation("io.mockk:mockk:1.12.4")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("com.github.tomakehurst:wiremock-jre8:2.33.2")
 }
 
 tasks.test {
