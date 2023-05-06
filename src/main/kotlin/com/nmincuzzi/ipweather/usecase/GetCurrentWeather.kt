54e9362bb5b316d5a5ab1471b9fcca95feca8874
Nicola Mincuzzi <48289901+NicoMincuzzi@users.noreply.github.com>
2023-05-06 17:11:11 +0200

    [chore]: Add a log example in GetCurrentWeather

--- src/main/kotlin/com/nmincuzzi/ipweather/usecase/GetCurrentWeather.kt
+++ src/main/kotlin/com/nmincuzzi/ipweather/usecase/GetCurrentWeather.kt
@@ -2,16 +2,19 @@ package com.nmincuzzi.ipweather.usecase
 
 import com.nmincuzzi.ipweather.infrastructure.OpenWeatherMapAdapter
 import com.nmincuzzi.ipweather.infrastructure.WeatherResponse
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class GetCurrentWeather(private val openWeatherMapAdapter: OpenWeatherMapAdapter) : Weather {
 
-    private val log = LoggerFactory.getLogger(GetCurrentWeather::class.java)
+    private val log: Logger = LoggerFactory.getLogger(GetCurrentWeather::class.java)
 
     override fun retrieveBy(city: String): WeatherResponse {
         val openWeatherMap = openWeatherMapAdapter.execute(city)
+        log.info("Current weather in {}: {}", city, openWeatherMap)
+
         return openWeatherMap.toWeatherRepresentation()
     }
 }
